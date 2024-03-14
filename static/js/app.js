f418b0e693c9ef6f0b90859294490568fe3dd754
StealthVigilante <155981063+StealthVigilante@users.noreply.github.com>
2024-03-14 23:33:47 +0530

    Add files via upload

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+<script>
+    // AngularJS module
+    var app = angular.module('fitnessTracker', []);
+
+    // AngularJS controller for handling frontend logic
+    app.controller('MainController', function($scope, $http) {
+        // Controller logic here
+    });
+</script>
